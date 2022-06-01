96f900d14dc11c5455510c94a8354998dc45cf04
Erinç Emre <erinc@192.168.1.52>
2022-06-01 18:44:29 +0300

    fix:	Navbar Bug and Auth setup has been fixed 	| 	+-> Due to Auth0 key probblem Navbar won't appear. I fixed the auth seups and now its working.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,7 +18,9 @@ export default function Navbar() {
   const { user, error, isLoading } = useUser();
 
   if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>{error.message}</div>;
+  if (error) {
+    console.log(error.message);
+  }
 
   if (user) {
     return (
